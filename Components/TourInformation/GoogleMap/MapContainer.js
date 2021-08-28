268805d3f7903b214f35571ae65f3b961ad8e5ef
Chris Enoiu <enoiucristinel@gmail.com>
2021-08-29 00:37:36 +0300

    Update Map Default Region

--- Components/TourInformation/GoogleMap/MapContainer.js
+++ Components/TourInformation/GoogleMap/MapContainer.js
@@ -1,25 +1,32 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import {StyleSheet, View, Dimensions} from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            <MapView style={styles.map} initialRegion={region}/>
         </View>
     );
 }
 
+const region = {
+    latitude: 45.429006386938994,
+    longitude: 28.025429868769756,
+    latitudeDelta: 0.1,
+    longitudeDelta: 0.1,
+}
+
 const styles = StyleSheet.create({
     container: {
-        zIndex:0,
-        position:"relative",
+        zIndex: 0,
+        position: "relative",
     },
     map: {
         alignItems: 'center',
         justifyContent: 'center',
         width: Dimensions.get('screen').width,
-        height: Dimensions.get('screen').height/100*70,
+        height: Dimensions.get('screen').height / 100 * 70,
     },
 });
 
